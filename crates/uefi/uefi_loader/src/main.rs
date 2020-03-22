8c49e50ff600fb001bdedf5b2b9c6609fb81a401
Dario Bartussek <d.bartussek@gmail.com>
2020-03-22 20:16:40 +0100

    It works by duplicating the code into the main crate. I have no idea what to do about this

--- crates/uefi/uefi_loader/src/main.rs
+++ crates/uefi/uefi_loader/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 #![feature(abi_efiapi)]
+#![feature(asm)]
+
 
 #[macro_use]
 extern crate alloc;
@@ -14,11 +16,11 @@ use crate::{
     load_elf::load_elf, memory_map::exit_boot_services,
     read_kernel::read_kernel,
 };
+use core::mem::{ManuallyDrop, MaybeUninit};
 use kernel_core::{exit, KernelArguments};
 use log::*;
 use page_table::KernelPageTable;
 use page_usage::PageUsage;
-use stack_switch::call_closure_with_stack;
 use uefi::{
     prelude::*,
     table::boot::{AllocateType, MemoryType},
@@ -38,6 +40,69 @@ const IDENTITY_BASE: u64 = KERNEL_ADDRESS_SPACE_BASE + KERNEL_REGION_SIZE * 0;
 const STACK_BASE: u64 = KERNEL_ADDRESS_SPACE_BASE + KERNEL_REGION_SIZE * 1;
 
 const STACK_SIZE_PAGES: u64 = 256;
+
+
+pub unsafe fn call_with_stack<T>(
+    arg: &mut T,
+    function: extern "sysv64" fn(&mut T) -> (),
+    stack: *mut u8,
+) {
+    asm!(r#"
+    mov rbp, rsp
+    mov rsp, $2
+
+    call $1
+
+    mov rsp, rbp
+    "#
+    : // Return values
+    : "{rdi}"(arg), "r"(function), "r"(stack) // Arguments
+    : "rbp", "cc", "memory" // Clobbers
+    : "volatile", "intel" // Options
+    );
+}
+
+/// Calls a closure and returns the result
+///
+/// This function is unsafe because it changes the stack pointer to stack.
+/// stack must be suitable to be used as a stack pointer on the target system.
+pub unsafe fn call_closure_with_stack<F, R>(closure: F, stack: *mut u8) -> R
+where
+    F: FnOnce() -> R,
+{
+    extern "sysv64" fn inner<F, R>(data: &mut (ManuallyDrop<F>, MaybeUninit<R>))
+    where
+        F: FnOnce() -> R,
+    {
+        let result = {
+            // Read the closure from context, taking ownership of it
+            let function = unsafe { ManuallyDrop::take(&mut data.0) };
+
+            // Call the closure.
+            // This consumes it and returns the result
+            function()
+        };
+
+        // Write the result into the context
+        data.1 = MaybeUninit::new(result);
+    }
+
+    // The context contains the closure and uninitialized memory for the return value
+    let mut context = (ManuallyDrop::new(closure), MaybeUninit::uninit());
+
+    call_with_stack(
+        &mut context,
+        // We create a new, internal function that does not close over anything
+        // and takes a context reference as its argument
+        inner,
+        stack,
+    );
+
+    // Read the result from the context
+    // No values are in the context anymore afterwards
+    context.1.assume_init()
+}
+
 
 #[entry]
 fn efi_main(image: Handle, st: SystemTable<Boot>) -> Status {
